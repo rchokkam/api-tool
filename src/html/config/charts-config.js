f42ffb9741d0728b70bc32c4e9cacbdce327ace6
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-11-22 15:30:10 +0100

    modify the configuration files

---
+++ src/html/config/charts-config.js
@@ -0,0 +1,5 @@
+{
+	"method_names" : ["GET","PUT","POST","DELETE"],
+	"status_codes" : [200,400,404,406,501],
+	"module_names" : ["abonnement","address","afsaetning","kunde","logistik","ordre","produkt","provisionering"]
+}
---
+++ src/html/config/charts-data.js
@@ -0,0 +1,89 @@
+{
+	"access_logs" : [
+		{
+			"method_name" : "GET",
+			"module_name" : "kunde",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "GET",
+			"module_name" : "kunde",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "GET",
+			"module_name" : "abonnement",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "GET",
+			"module_name" : "kunde",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "GET",
+			"module_name" : "afsaetning",
+			"status_code" : 501
+		},
+		{
+			"method_name" : "PUT",
+			"module_name" : "kunde",
+			"status_code" : 501
+		},
+		{
+			"method_name" : "POST",
+			"module_name" : "kunde",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "POST",
+			"module_name" : "kunde",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "DELETE",
+			"module_name" : "kunde",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "DELETE",
+			"module_name" : "address",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "GET",
+			"module_name" : "address",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "GET",
+			"module_name" : "address",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "PUT",
+			"module_name" : "address",
+			"status_code" : 200
+		},
+		{
+			"method_name" : "PUT",
+			"module_name" : "address",
+			"status_code" : 501
+		},
+		{
+			"method_name" : "PUT",
+			"module_name" : "address",
+			"status_code" : 501
+		},
+		{
+			"method_name" : "POST",
+			"module_name" : "address",
+			"status_code" : 501
+		},
+		{
+			"method_name" : "DELETE",
+			"module_name" : "kunde",
+			"status_code" : 501
+		}
+	]
+}
